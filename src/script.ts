1fa4d62eec543b5920f4129567d133c15d72cf2e
al-jabir <aljabir098@gmail.com>
2022-11-14 07:20:16 +0600

    Type basic done clear

--- src/script.ts
+++ src/script.ts
@@ -1,3 +1,51 @@
-const ts = 'Learning TypeScript';
-console.log(ts);
-// kalke learning korbo sokal
+// Type Basics
+
+let names;
+names = 'mafia';
+names = 'Knight';
+
+console.log(names);
+
+// function
+
+function multiple(a: number, b: number) {
+  return a * b;
+}
+
+console.log(multiple(5, 9));
+
+//Array
+
+let fruits = ['apple', 'banana', 'orange'];
+
+fruits.push('lemon');
+
+let arrData = [];
+
+arrData.push(1998);
+arrData.push('Jabir');
+
+let mixed = ['mafia', 19, true];
+
+mixed.push(false);
+mixed.push('king');
+// mixed.push({
+//   name: 'Gangster', /* cant be work */
+// });
+
+console.log(arrData);
+
+console.log(mixed);
+
+// object
+
+let person = {
+  name: 'Sarah Knight',
+  age: 19,
+  job: 'deveoper',
+};
+person.job = 'Senior Software Engineer';
+
+person.isCountry = 'Munich,German'; // can't not working
+
+console.log(person.job);
